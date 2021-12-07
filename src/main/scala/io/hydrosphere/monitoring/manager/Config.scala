760f342371bd964c16f060948430b4d8f7e02809
Pavel Makarichev <vixtir90@gmail.com>
2021-12-07 11:43:37 +0300

    fix: increase httpMaxRequestSize to 8M (#18)

--- src/main/scala/io/hydrosphere/monitoring/manager/Config.scala
+++ src/main/scala/io/hydrosphere/monitoring/manager/Config.scala
@@ -13,7 +13,7 @@ import zio.config.typesafe.TypesafeConfigSource
 case class EndpointConfig(
     httpHost: java.net.URI,
     httpPort: Int,
-    httpMaxRequestSize: Int = 2048,
+    httpMaxRequestSize: Int = 8192,
     grpcPort: Int
 ) {
   lazy val httpUri = {
